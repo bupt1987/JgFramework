43bd847b4880d5beceb8f7f1cd11b5b30f54d5e7
俊杰Jerry <bupt1987@gmail.com>
2013-12-06 16:49:58 +0800

    更新sercret的默认验证

--- src/main/java/com/zhaidaosi/game/jgframework/session/BaseSercretFactory.java
+++ src/main/java/com/zhaidaosi/game/jgframework/session/BaseSercretFactory.java
@@ -1,6 +1,5 @@
 package com.zhaidaosi.game.jgframework.session;
 
-import com.zhaidaosi.game.jgframework.common.BaseDate;
 import com.zhaidaosi.game.jgframework.common.encrpt.BaseRsa;
 import com.zhaidaosi.game.jgframework.common.excption.BaseException;
 
@@ -8,18 +7,20 @@ public class BaseSercretFactory implements IBaseSercretFactory {
 
 	@Override
 	public String createSercret(int userId) throws BaseException {
-		String time = BaseDate.time2String(BaseDate.FORMAT_YY_MM_DD_HH_MM_SS);
-		String input = userId + "_" + time;
-		String bytes = BaseRsa.encrypt(input);
-		if(bytes == null){
+		String input = userId + "_" + System.currentTimeMillis();
+		String sercret = BaseRsa.encrypt(input);
+		if(sercret == null){
 			throw new BaseException("秘钥生成失败", 100);
 		}
-		return bytes;
+		return sercret;
 	}
 
 	@Override
 	public int checkSercret(String sercret) throws Exception {
 		sercret = BaseRsa.decrypt(sercret);
+		if(sercret == null){
+			throw new BaseException("非法秘钥", 101);
+		}
 		String[] arr = sercret.split("_");
 		if(arr.length != 2){
 			throw new BaseException("非法秘钥", 101);
@@ -29,7 +30,7 @@ public class BaseSercretFactory implements IBaseSercretFactory {
 			throw new BaseException("非法秘钥", 101);
 		}
 		//有效期1分钟
-		if(BaseDate.string2Time(arr[1]) - System.currentTimeMillis() > 60000){
+		if(Long.parseLong(arr[1]) - System.currentTimeMillis() > 60000){
 			throw new BaseException("秘钥已失效", 102);
 		}
 		return userId;
